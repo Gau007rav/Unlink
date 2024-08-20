a98bd4433f09bfd210986c7fe51bd935e4b50c44
Saurabh <SaurabhSingh@Saurabhs-MacBook-Air.local>
2024-08-20 18:27:01 +0530

    contactMeChanges

--- src/components/Contactme.js
+++ src/components/Contactme.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import LinksSection from "./LinksSectiom";
-
+import "../App.css";
 function CardsSection() {
   useEffect(() => {
     const cards = document.querySelectorAll(".card");
@@ -21,43 +21,56 @@ function CardsSection() {
       <div className="text-lg md:text-xl font-thin text-white mb-4 transform transition-transform duration-300 hover:scale-110">
         gaurav033singh@gmail.com
       </div>
-
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-x-16">
-        <div className="card bg-gray-800 text-white p-6 rounded-lg shadow-lg transform transition-transform duration-300 opacity-0 translate-y-4 hover:scale-125">
-          <h2 className="text-xl lg:text-2xl font-bold mb-4">Education</h2>
-          <p className="font-serif text-lg">
-            M-tech in computer science and engineering
-          </p>
-          <p className="font-serif text-lg">
-            B-tech in computer science and engineering
-          </p>
-          <p className="font-serif text-lg">12 from CBSE board with PCM</p>
-          <p className="font-serif text-lg">10 from CBSE board</p>
+        <div class="card">
+          <h1 className="font-bold text-xl">Education</h1>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"></svg>
+          <div class="card__content">
+            <p class="card__title">Education</p>
+            <p class="card__description">
+              <ul className="text-black">
+                <li>M-tech in Computer Science and Engineering</li>
+                <li>B-tech in Computer Science and Engineering</li>
+                <li>12 from CBSE Board PCM</li>
+                <li>10 from CBSE board</li>
+              </ul>
+            </p>
+          </div>
         </div>
-        <div className="card bg-gray-800 text-white p-6 rounded-lg shadow-lg transform transition-transform duration-300 opacity-0 translate-y-4 hover:scale-125">
-          <h2 className="text-xl lg:text-2xl font-bold mb-4">Skills</h2>
-          <p className="font-serif text-lg">Languages: JavaScript, Python</p>
-          <p className="font-serif text-lg">
-            Frameworks: React.js, Node.js, Express.js
-          </p>
-          <p className="font-serif text-lg">HTML, CSS, Git, GitHub</p>
-          <p className="font-serif text-lg">Database:MongoDB,MySql</p>
+        <div class="card">
+          <h1 className="font-bold text-xl">Skills</h1>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"></svg>
+          <div class="card__content">
+            <p class="card__title">Skills</p>
+            <p class="card__description">
+              <ul className="text-black">
+                <li>Languages:Javascript,Python</li>
+                <li>Framwork:React.js,Express.js,Node.js</li>
+                <li>Others:HTML,CSS,Git,Github</li>
+                <li>Database:MongoDb,MySql</li>
+              </ul>
+            </p>
+          </div>
         </div>
-        <div className="card bg-gray-800 text-white p-6 rounded-lg shadow-lg transform transition-transform duration-300 opacity-0 translate-y-4 hover:scale-125">
-          <h2 className="text-xl lg:text-2xl font-bold mb-4">Experience</h2>
-          <p className="font-serif text-lg">
-            I have accumulated 2 years of experience in front-end development
-            and an additional year in backend development, primarily within the
-            e-commerce domain. During this time, I worked at Rastech Digital, a
-            startup where I successfully completed two significant projects. My
-            responsibilities included developing and maintaining landing pages
-            and product description pages using React, as well as integrating
-            data from APIs to dynamically display content on the user interface.
-            In addition to my practical experience, I have enhanced my skills
-            through a Full Stack Development Bootcamp at Newton School, where I
-            deepened my understanding of modern development practices and
-            technologies.
-          </p>
+
+        <div class="card">
+          <h1 className="font-bold text-xl">Experience</h1>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"></svg>
+          <div class="card__content text-black">
+            <p class="card__title text-black">Experience</p>
+            <p className="card__description text-black">
+              I have accumulated 2 years of experience in front-end development
+              and an additional year in backend development, primarily within
+              the e-commerce domain. During this time, I worked at Rastech
+              Digital, a startup where I successfully completed two significant
+              projects. My responsibilities included developing and maintaining
+              landing pages and product description pages using React, as well
+              as integrating data from APIs.
+            </p>
+          </div>
         </div>
       </div>
       <div className="links">
